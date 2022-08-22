994b45076eb8723341962d3796a52ea841995ac0
Luis Paz <lpaz8806@gmail.com>
2022-08-22 07:17:01 +0200

    skeleton

---
+++ src/main/java/se/jensensthlm/forexgump/ForexGumpApplication.java
@@ -0,0 +1,13 @@
+package se.jensensthlm.forexgump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexGumpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForexGumpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/jensensthlm/forexgump/ForexGumpApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensensthlm.forexgump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexGumpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
